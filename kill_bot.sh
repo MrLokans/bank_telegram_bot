c8c31d3c24c114025e8d96f71e437729e02f1f34
MrLokans <trikster1911@gmail.com>
2016-04-18 18:41:57 +0300

    Bot presence condition add to bot killer script.

--- kill_bot.sh
+++ kill_bot.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
 bot_pid=$(ps aux | grep 'python bot.py$' | cut -d' ' -f2)
-kill -9 $bot_pid
+
+if [ -z $bot_pid ]
+then
+	echo "Bot process is not found!"	
+else
+	kill -9 $bot_pid
+fi
+
