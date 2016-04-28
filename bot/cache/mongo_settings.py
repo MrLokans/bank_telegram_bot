c606184a6612a8823bd8e8c6141001d53eda768b
MrLokans <trikster1911@gmail.com>
2016-04-28 22:41:29 +0300

    Added default settings for mongodb cache. Fixes #5.

--- bot/cache/mongo_settings.py
+++ bot/cache/mongo_settings.py
@@ -1,8 +1,8 @@
 import os
 
-MONGO_HOST = os.environ.get('TELEGRAM_MONGODB_HOST')
+MONGO_HOST = os.environ.get('TELEGRAM_MONGODB_HOST', 'localhost')
 MONGO_PORT = int(os.environ.get('TELEGRAM_MONGODB_PORT', 27017))
-MONGO_DATABASE = os.environ.get('TELEGRAM_MONGODB_DB')
-MONGO_COLLECTION = os.environ.get('TELEGRAM_MONGODB_COLLECTION')
+MONGO_DATABASE = os.environ.get('TELEGRAM_MONGODB_DB', 'telegrambot')
+MONGO_COLLECTION = os.environ.get('TELEGRAM_MONGODB_COLLECTION', 'currencies')
 MONGO_USER = os.environ.get('TELEGRAM_MONGODB_USER')
 MONGO_PASSWORD = os.environ.get('TELEGRAM_MONGODB_PASSWORD')
