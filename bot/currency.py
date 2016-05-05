efa7e911ee80bc4efa2cecbaf36a0693466194bf
MrLokans <trikster1911@gmail.com>
2016-05-05 21:28:46 +0300

    Currency turned into object (memory-efficient)

--- bot/currency.py
+++ bot/currency.py
@@ -1,3 +1,23 @@
-from collections import namedtuple
+from typing import Union
 
-Currency = namedtuple('Currency', "name iso sell buy")
+
+class Currency(object):
+    __slots__ = ('name', 'iso', 'buy', 'sell')
+
+    def __init__(self, name: str="",
+                 iso: str="",
+                 sell: Union[float, int]=None,
+                 buy: float=None):
+        self.name = name
+        self.iso = iso
+        self.sell = sell
+        self.buy = buy
+
+    def __repr__(self) -> str:
+        s = "Currency({}, {}, {}, {})"
+        return s.format(self.name, self.iso, self.sell, self.buy)
+
+    def __str__(self) -> str:
+        return "<Currency {}: {} : {}>".format(self.iso,
+                                               self.sell,
+                                               self.buy if self.buy else "-")
